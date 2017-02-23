0baea830cdf57ed0e45d9089f11db658178267f7
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-02-24 06:15:33 +1100

    Added initial configuration implementation and then realized to test it, I will need to build the actual NuGet package. Moving to configuring the build server.

--- NUnitTestTimeLimiter/ModuleWeaver.cs
+++ NUnitTestTimeLimiter/ModuleWeaver.cs
@@ -4,21 +4,25 @@ using NUnit.Framework;
 using NUnitTestTimeLimiter.Fody;
 using System;
 using System.Diagnostics;
+using System.Xml.Linq;
 using MoreLinq;
 
 // ReSharper disable once CheckNamespace => This is the convention suggested by the BasicFodyAddin Fody template.
 public class ModuleWeaver
 {
-    private const int TimeLimit = 2000;
+    private const int DefaultTimeLimit = 2000;
+    private static int _timeLimit = DefaultTimeLimit;
+
+    public XElement Config { get; set; }
 
     // Will log an informational message to MSBuild
-    // ReSharper disable once MemberCanBePrivate.Global => Fody will probably use this when calling our module.
+    // ReSharper disable once MemberCanBePrivate.Global => Fody will use this when calling our module.
     [NotNull]
     public Action<string> LogInfo { get; }
 
     // An instance of Mono.Cecil.ModuleDefinition for processing
-    // ReSharper disable once MemberCanBePrivate.Global => Fody will probably use this when calling our module.
-    // ReSharper disable once UnusedAutoPropertyAccessor.Global => Fody will probably use this when calling our module.
+    // ReSharper disable once MemberCanBePrivate.Global => Fody will use this when calling our module.
+    // ReSharper disable once UnusedAutoPropertyAccessor.Global => Fody will use this when calling our module.
     public ModuleDefinition ModuleDefinition { get; set; }
 
     // Init logging delegates to make testing easier
@@ -38,11 +42,11 @@ public class ModuleWeaver
         var attributeConstructor =
             moduleDefinition.ImportReference(typeof(TimeoutAttribute).GetConstructor(new[] { typeof(int) }));
         var attribute = new CustomAttribute(attributeConstructor);
-        attribute.ConstructorArguments?.Add(new CustomAttributeArgument(moduleDefinition.TypeSystem?.Int32, TimeLimit));
+        attribute.ConstructorArguments?.Add(new CustomAttributeArgument(moduleDefinition.TypeSystem?.Int32, _timeLimit));
         typeDefinition.CustomAttributes?.Add(attribute);
     }
 
-    private void SetTimeout([NotNull] ModuleDefinition moduleDefinition, TypeDefinition typeDefinition)
+    private static void SetTimeout([NotNull] ModuleDefinition moduleDefinition, TypeDefinition typeDefinition)
     {
         if (typeDefinition == null)
         {
@@ -65,7 +69,7 @@ public class ModuleWeaver
     {
         var timeoutAttribute = moduleDefinition.ImportReference(typeof(TimeoutAttribute));
         var attritube = typeDefinition.Attribute(timeoutAttribute);
-        var maxTimeLimitArgument = new CustomAttributeArgument(moduleDefinition.TypeSystem?.Int32, TimeLimit);
+        var maxTimeLimitArgument = new CustomAttributeArgument(moduleDefinition.TypeSystem?.Int32, _timeLimit);
         if (attritube?.ConstructorArguments == null)
         {
             return;
@@ -79,7 +83,7 @@ public class ModuleWeaver
 
         var constructorArgument = attritube.ConstructorArguments[0];
         var intValue = (int?)constructorArgument.Value;
-        if (!intValue.HasValue || intValue.Value <= TimeLimit)
+        if (!intValue.HasValue || intValue.Value <= _timeLimit)
         {
             return;
         }
@@ -92,6 +96,16 @@ public class ModuleWeaver
     {
         try
         {
+            var configurationAttributes = Config.Attributes();
+            foreach (var a in configurationAttributes)
+            {
+                int timeLimit;
+                if (a.Name == "TimeLimit" && int.TryParse(a.Value, out timeLimit))
+                {
+                    _timeLimit = timeLimit;
+                }
+            }
+
             var moduleDefinition = ModuleDefinition;
             if (moduleDefinition == null)
             {
